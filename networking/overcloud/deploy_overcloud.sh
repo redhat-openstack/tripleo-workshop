098841fd36804632718e6a23eaefeafe27d5a022
hjensas <hjensas@redhat.com>
2018-06-04 17:34:07 +0200

    Use renderend network-environment

--- networking/overcloud/deploy_overcloud.sh
+++ networking/overcloud/deploy_overcloud.sh
@@ -1,5 +1,6 @@
 source /home/stack/stackrc
 
+
 openstack overcloud deploy \
   --templates \
   --ntp-server clock.redhat.com \
@@ -9,9 +10,29 @@ openstack overcloud deploy \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
-  -e /home/stack/environments/network-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+  -e /home/stack/environments/network-environment-overrides.yaml \
+  -e /home/stack/environments/service_net_map_overrides.yaml \
   -e /home/stack/environments/scheduler_hints_env.yaml \
   -e /home/stack/environments/custom_hostnames.yaml \
   -e /home/stack/environments/predictable_ips.yaml \
   -e /home/stack/templates/ceph/ceph.yaml \
   -e /home/stack/environments/docker_registry.yaml
+
+
+
+#openstack overcloud deploy \
+#  --templates \
+#  --ntp-server clock.redhat.com \
+#  -n /home/stack/templates/network_data.yaml \
+#  -r /home/stack/templates/roles_data.yaml \
+#  -e /home/stack/environments/nodes_data.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
+#  -e /home/stack/environments/network-environment.yaml \
+#  -e /home/stack/environments/scheduler_hints_env.yaml \
+#  -e /home/stack/environments/custom_hostnames.yaml \
+#  -e /home/stack/environments/predictable_ips.yaml \
+#  -e /home/stack/templates/ceph/ceph.yaml \
+#  -e /home/stack/environments/docker_registry.yaml
