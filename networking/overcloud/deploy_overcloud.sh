9f4d85a5ef566e41fcb4eddfa33a7613b1411be9
hjensas <hjensas@redhat.com>
2018-06-05 08:58:31 +0200

    ...

--- networking/overcloud/deploy_overcloud.sh
+++ networking/overcloud/deploy_overcloud.sh
@@ -1,4 +1,5 @@
 source /home/stack/stackrc
+cd /home/stack
 
 
 openstack overcloud deploy \
@@ -18,21 +19,3 @@ openstack overcloud deploy \
   -e /home/stack/environments/predictable_ips.yaml \
   -e /home/stack/templates/ceph/ceph.yaml \
   -e /home/stack/environments/docker_registry.yaml
-
-
-
-#openstack overcloud deploy \
-#  --templates \
-#  --ntp-server clock.redhat.com \
-#  -n /home/stack/templates/network_data.yaml \
-#  -r /home/stack/templates/roles_data.yaml \
-#  -e /home/stack/environments/nodes_data.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
-#  -e /home/stack/environments/network-environment.yaml \
-#  -e /home/stack/environments/scheduler_hints_env.yaml \
-#  -e /home/stack/environments/custom_hostnames.yaml \
-#  -e /home/stack/environments/predictable_ips.yaml \
-#  -e /home/stack/templates/ceph/ceph.yaml \
-#  -e /home/stack/environments/docker_registry.yaml
